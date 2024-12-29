bda5541c7190535146f9f0cbf60b585acd2f8278
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2024-12-29 17:41:48 +0530

    Time: 22 ms (20.68%) | Memory: 59.9 MB (10.69%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.java
@@ -1,43 +1,26 @@
 class Solution {
     public long dividePlayers(int[] skill) {
-        int[] countArr = new int[2001];
+        Map<Integer, Integer> countMap = new HashMap<>();
         int i, sum;
         sum = 0;
         for(i = 0; i < skill.length; i++) {
             sum += skill[i];
-            countArr[skill[i]]++;
+            countMap.put(skill[i], countMap.getOrDefault(skill[i], 0) + 1);
         }
         if(sum % (skill.length / 2) != 0) {
             return -1L;
         }
-        int skillValue = sum / (skill.length / 2);
+        int target = sum / (skill.length / 2);
         long chemValue = 0L;
-        for(i = 1; i <= skillValue / 2; i++) {
-            if(countArr[i] != 0 && countArr[skillValue - i] != 0) {
-                if(countArr[i] == countArr[skillValue - i]) {
-                    if(i * 2 == skillValue) {
-                        if(countArr[i] % 2 == 0) {
-                            chemValue += 1L * i * i * (countArr[i] / 2);
-                        }
-                        else {
-                            return -1L;
-                        }
-                    }
-                    else {
-                        chemValue += 1L * i * (skillValue - i) * countArr[i];
-                    }
-                }
-                else {
-                    return -1L;
-                }
-            }
-            else if(countArr[i] == 0 && countArr[skillValue - i] == 0) {
-                continue;
-            }
-            else {
+        for(Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if(!countMap.containsKey(target - entry.getKey()) 
+                || !(countMap.get(entry.getKey()).equals(countMap.get(target - entry.getKey())))) {
                 return -1L;
             }
+            else {
+                chemValue +=  1L * entry.getKey() * (target - entry.getKey()) * entry.getValue();
+            }
         }
-        return chemValue;
+        return chemValue / 2;
     }
 }
