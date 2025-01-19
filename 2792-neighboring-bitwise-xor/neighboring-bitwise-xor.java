44fe0e66607096fd8b725c2575a52a0177c95602
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2025-01-19 09:37:57 +0530

    Time: 2 ms (100.00%) | Memory: 62.5 MB (46.52%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -3,9 +3,7 @@ class Solution {
         int last, i;
         last = 0;
         for(i = 0; i < derived.length; i++) {
-            if(derived[i] == 1) {
-                last = ~last;
-            }
+           last = last ^ derived[i];
         }
         return last == 0;
     }
