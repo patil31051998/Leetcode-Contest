2af8f35624a6a61e5a1729e19aaa7cdd001d8994
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2025-01-23 10:35:09 +0530

    Time: 3 ms (47.43%) | Memory: 53 MB (9.06%) - LeetSync

--- 1396-count-servers-that-communicate/count-servers-that-communicate.java
+++ 1396-count-servers-that-communicate/count-servers-that-communicate.java
@@ -1,35 +1,24 @@
 class Solution {
     public int countServers(int[][] grid) {
-        int[][] visited = new int[grid.length][grid[0].length];
+        int[] rowCount = new int[grid.length];
+        int[] colCount = new int[grid[0].length];
         int i, j, count;
         count = 0;
-        for(i = 0; i < visited.length; i++) {
-            for(j = 0; j < visited[0].length; j++) {
-                if(grid[i][j] == 1 && visited[i][j] == 0) {
-                    int val = dfs(grid, i, j, visited);
-                    count += (val > 0 ? val + 1 : val);
+        for(i = 0; i < grid.length; i++) {
+            for(j = 0; j < grid[0].length; j++) {
+                if(grid[i][j] == 1) {
+                    rowCount[i]++;
+                    colCount[j]++;
                 }
             }
         }
-        return count;
-    }
-
-    private int dfs(int[][] grid, int i, int j, int[][] visited) {
-        visited[i][j] = 1;
-        int x, count;
-        count = 0;
-        for(x = 0; x < grid.length; x++) {
-            if(grid[x][j] == 1 && visited[x][j] == 0) {
-                count++;
-                count += dfs(grid, x, j, visited);
+         for(i = 0; i < grid.length; i++) {
+            for(j = 0; j < grid[0].length; j++) {
+                if((grid[i][j] == 1) && (rowCount[i] > 1 || colCount[j] > 1)) {
+                    count++;
+                }
             }
-        }
-        for(x = 0; x < grid[0].length; x++) {
-            if(grid[i][x] == 1 && visited[i][x] == 0) {
-                count++;
-                count += dfs(grid, i, x, visited);
-            }
-        }
+         }
         return count;
     }
 }
