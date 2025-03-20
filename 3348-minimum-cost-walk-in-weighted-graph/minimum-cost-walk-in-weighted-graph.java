036dfbd20400fe225e61fb2eca09963c52956439
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2025-03-20 19:05:25 +0530

    Time: 41 ms (10.59%) | Memory: 126.5 MB (20.00%) - LeetSync

--- 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
+++ 3348-minimum-cost-walk-in-weighted-graph/minimum-cost-walk-in-weighted-graph.java
@@ -1,97 +1,55 @@
 class Solution {
-
-    public int[] minimumCost(int n, int[][] edges, int[][] queries) {
-        // Create the adjacency list of the graph
-        List<List<int[]>> adjList = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            adjList.add(new ArrayList<>(2));
+    public int[] minimumCost(int n, int[][] edges, int[][] query) {
+        List<List<int[]>> adjList = getAdjList(n, edges);
+        for(int[] data : adjList.get(1)) {
+            System.out.println(Arrays.toString(data));
         }
-        for (int[] edge : edges) {
-            adjList.get(edge[0]).add(new int[] { edge[1], edge[2] });
-            adjList.get(edge[1]).add(new int[] { edge[0], edge[2] });
-        }
-
-        boolean[] visited = new boolean[n];
-
-        // Array to store the component ID of each node
-        int[] components = new int[n];
-        List<Integer> componentCost = new ArrayList<>(n);
-
-        int componentId = 0;
-
-        // Perform DFS for each unvisited node to identify components and calculate their costs
-        for (int node = 0; node < n; node++) {
-            // If the node hasn't been visited, it's a new component
-            if (!visited[node]) {
-                // Get the component cost and mark all nodes in the component
-                componentCost.add(
-                    getComponentCost(
-                        node,
-                        adjList,
-                        visited,
-                        components,
-                        componentId
-                    )
-                );
-                // Increment the component ID for the next component
-                componentId++;
+        Map<Integer, Integer> componentValMap = new HashMap<>();
+        int[] component = new int[n];
+        int[] res = new int[query.length];
+        int i;
+        int currComponent = 1;
+        for(i = 0; i < n; i++) {
+            if(component[i] == 0) {
+                int[] val = {Integer.MAX_VALUE};
+                dfs(i, adjList, currComponent, val, component);
+                componentValMap.put(currComponent, val[0]);
+                currComponent++;
             }
         }
-
-        int[] answer = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int start = queries[i][0];
-            int end = queries[i][1];
-
-            if (components[start] == components[end]) {
-                // If they are in the same component, return the precomputed cost for the component
-                answer[i] = componentCost.get(components[start]);
-            } else {
-                // If they are in different components, return -1
-                answer[i] = -1;
+        for(i = 0; i < query.length; i++) {
+            if(component[query[i][0]] != component[query[i][1]]) {
+                res[i] = -1;
+            }
+            else {
+                res[i] = componentValMap.get(component[query[i][0]]);
             }
         }
-
-        return answer;
+        return res;
     }
 
-    // Helper function to calculate the cost of a component using BFS
-    private int getComponentCost(
-        int node,
-        List<List<int[]>> adjList,
-        boolean[] visited,
-        int[] components,
-        int componentId
-    ) {
-        // Initialize the cost to the number that has only 1s in its
-        // binary representation
-        int currentCost = Integer.MAX_VALUE;
-
-        // Mark the node as part of the current component
-        components[node] = componentId;
-        visited[node] = true;
-
-        // Explore all neighbors of the current node
-        for (int[] neighbor : adjList.get(node)) {
-            int weight = neighbor[1];
-
-            // Update the cost by performing a bitwise AND of the edge
-            // weights
-            currentCost &= weight;
-
-            if (!visited[neighbor[0]]) {
-                // Recursively calculate the cost of the rest of the component
-                // and accumulate it into currentCost
-                currentCost &= getComponentCost(
-                    neighbor[0],
-                    adjList,
-                    visited,
-                    components,
-                    componentId
-                );
+    private void dfs(int i, List<List<int[]>> adjList, int currComponent, int[] val, int[] component) {
+        component[i] = currComponent;
+        for(int[] adj : adjList.get(i)) {
+            val[0] = val[0] & adj[1];
+            if(component[adj[0]] != 0) {
+                continue;
             }
+            dfs(adj[0], adjList, currComponent, val, component);
         }
 
-        return currentCost;
     }
+
+    private List<List<int[]>> getAdjList(int n, int[][] edges) {
+        int i;
+        List<List<int[]>> adjList = new ArrayList<>();
+        for(i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+        for(int[] edge : edges) {
+            adjList.get(edge[0]).add(new int[]{edge[1], edge[2]});
+            adjList.get(edge[1]).add(new int[]{edge[0], edge[2]});
+        }
+        return adjList;
+    } 
 }
