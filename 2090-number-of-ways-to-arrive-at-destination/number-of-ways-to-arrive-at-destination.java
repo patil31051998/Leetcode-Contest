26a0befc5eedea4479531629b355853403dda3f5
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2025-03-24 10:55:30 +0530

    Time: 21 ms (6.81%) | Memory: 49.1 MB (78.22%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -0,0 +1,66 @@
+
+
+class Solution {
+
+    class Data {
+    private int node;
+    private long dist;
+
+    public Data(int node, long dist) {
+        this.node = node;
+        this.dist = dist;
+    }
+
+    public int getNode() {
+        return node;
+    }
+
+    public long getdist() {
+        return this.dist;
+    } 
+
+}
+
+    private final int MOD = (int)(Math.pow(10, 9) + 7);
+
+    public int countPaths(int n, int[][] roads) {
+        List<List<int[]>> adjList = getAdjList(n, roads);
+        long[] distance = new long[n];
+        int[] ways = new int[n];
+        Arrays.fill(distance, Long.MAX_VALUE);
+        distance[0] = 0;
+        ways[0] = 1;
+        PriorityQueue<Data> queue = new PriorityQueue<>((d1, d2) -> {
+            return (int)(d1.getdist() - d2.getdist());
+        });
+        queue.add(new Data(0, 0));
+        while(!queue.isEmpty()) {
+            Data curr = queue.remove();
+            for(int[] adj : adjList.get(curr.getNode())) {
+                
+                if(distance[adj[0]] > curr.getdist() + adj[1]) {
+                    ways[adj[0]] = ways[curr.getNode()];
+                    distance[adj[0]] = curr.getdist() + adj[1];
+                    queue.add(new Data(adj[0], distance[adj[0]]));
+                }
+                else if(distance[adj[0]] == curr.getdist() + adj[1]) {
+                    ways[adj[0]] = (ways[adj[0]] + ways[curr.getNode()]) % MOD;
+                }
+            }
+        }
+        return ways[n - 1];
+    }
+
+    private List<List<int[]>> getAdjList(int n, int[][] roads) {
+        List<List<int[]>> adjList = new ArrayList<>();
+        int i;
+        for(i = 0; i < n; i++) {
+            adjList.add(new ArrayList());
+        }
+        for(int[] road : roads) {
+            adjList.get(road[0]).add(new int[] { road[1], road[2]});
+            adjList.get(road[1]).add(new int[] { road[0], road[2]});
+        }
+        return adjList;
+    }
+}
