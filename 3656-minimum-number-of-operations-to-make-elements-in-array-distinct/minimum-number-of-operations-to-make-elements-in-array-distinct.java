2272f8ad45ac260b0614f9527c710206793b3c23
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2025-04-08 08:26:31 +0530

    Time: 4 ms (29.35%) | Memory: 44.3 MB (73.87%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.java
@@ -1,31 +1,27 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        int i, j;
-        int[] count = new int[101];
-        int res = 0;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        Set<Integer> dupSet = new HashSet<>();
+        int i, res;
+        res = 0;
         for(i = 0; i < nums.length; i++) {
-            count[nums[i]]++;
+            countMap.put(nums[i], countMap.getOrDefault(nums[i], 0) + 1);
+            if(countMap.get(nums[i]) > 1) {
+                dupSet.add(nums[i]);
+            }
         }
-        for(i = 0; i < nums.length; i = i + 3) {
-            if(isUnique(count)) {
-                return res;
+        i = 0;
+        while(!dupSet.isEmpty()) {
+            int j = i;
+            for(; j < i + 3 && j < nums.length; j++) {
+                countMap.put(nums[j], countMap.get(nums[j]) - 1);
+                if(countMap.get(nums[j]) == 1) {
+                    dupSet.remove(nums[j]);
+                }
             }
-            for(j = i; j < i + 3 && j < nums.length; j++) {
-                count[nums[j]]--;
-            }
+            i = i + 3;
             res++;
         }
-        
-        return res;        
-    }
-    
-    private boolean isUnique(int[] count) {
-        int i;
-        for(i = 0; i < count.length; i++) {
-            if(count[i] > 1) {
-                return false;
-            }
-        }
-        return true;
+        return res;
     }
 }
