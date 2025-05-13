851362ef1781e8e74c261e14079a02a99fb48b0d
patil31051998 <145420889+patil31051998@users.noreply.github.com>
2025-05-13 16:00:44 +0530

    Time: 103 ms (56.96%) | Memory: 45.9 MB (29.11%) - LeetSync

---
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    private final static int MOD = (int)Math.pow(10, 9) + 7;
+
+    public int lengthAfterTransformations(String s, int t) {
+        long[] currCount = new long[26];
+        int i, j;
+        long res = 0L;
+        for(i = 0; i < s.length(); i++) {
+            currCount[s.charAt(i) - 'a']++;
+        }
+        for(i = 1; i <= t; i++) {
+            long[] newCount = new long[26];
+            for(j = 0; j < 25; j++) {
+                newCount[j + 1] = currCount[j];
+            }
+            newCount[0] = currCount[25];
+            newCount[1] = (newCount[1] + currCount[25]) % MOD;
+            currCount = newCount;
+        }
+        for(i = 0; i < 26; i++) {
+            res = (res + currCount[i]) % MOD;
+        }
+        return (int)res;
+    }
+}
